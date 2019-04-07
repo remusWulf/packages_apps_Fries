79ab2fc3a2953aea03232cee63b3b30b57ac5594
remusWulf <remuswulf@hotmail.com>
2019-04-07 20:20:50 +0100

    added missing Preferences
    Change-Id: I66aa5b1f764a341925ad1ab58db8458b6f0f67d6

--- src/com/potato/fries/preferences/CustomSeekBarPreference.java
+++ src/com/potato/fries/preferences/CustomSeekBarPreference.java
@@ -38,7 +38,7 @@ public class CustomSeekBarPreference extends Preference implements SeekBar.OnSee
     private int mInterval = 1;
     private int mCurrentValue;
     private int mDefaultValue = -1;
-    private int mMax = 100;
+    private int mMax = 255;
     private String mUnits = "";
     private String mDefaultText = "";
     private SeekBar mSeekBar;
@@ -51,9 +51,12 @@ public class CustomSeekBarPreference extends Preference implements SeekBar.OnSee
         final TypedArray a = context.obtainStyledAttributes(
                 attrs, R.styleable.CustomSeekBarPreference);
 
-        mMax = attrs.getAttributeIntValue(ANDROIDNS, "max", 100);
+        mMax = attrs.getAttributeIntValue(ANDROIDNS, "max", 255);
         mMin = attrs.getAttributeIntValue(SETTINGS_NS, "min", 0);
         mDefaultValue = attrs.getAttributeIntValue(ANDROIDNS, "defaultValue", -1);
+        if (mDefaultValue > mMax) {
+            mDefaultValue = mMax;
+        }
         mUnits = getAttributeStringValue(attrs, SETTINGS_NS, "units", "");
         mDefaultText = getAttributeStringValue(attrs, SETTINGS_NS, "defaultText", "Def");
 
@@ -141,7 +144,7 @@ public class CustomSeekBarPreference extends Preference implements SeekBar.OnSee
             mStatusText.setText(mDefaultText);
         } else {
             mStatusText.setText(String.valueOf(mCurrentValue) + mUnits);
-        } 
+        }
         mSeekBar.setProgress(mCurrentValue - mMin);
         mTitle = (TextView) view.findViewById(android.R.id.title);
 
